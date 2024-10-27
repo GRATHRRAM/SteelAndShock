b31cc4f8aa427658ec4644c24df5e4d5a9394de3
Grah <101575465+GRATHRRAM@users.noreply.github.com>
2024-10-27 11:02:29 +0100

    looking better but some colision issiues

---
+++ assets/objects/mapobj/bush.go
@@ -0,0 +1,11 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"bush1\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/assets/objects/mapobj/mapopbja.atlas\"\n"
+  "}\n"
+  ""
+}
